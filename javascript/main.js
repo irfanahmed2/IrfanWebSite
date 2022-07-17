e09a97875a890ed87354a91e671add6dcd26be44
irfanboy999 <irfanahmedok169@gmail.com>
2022-07-17 19:01:24 +0500

    Summary
    To show my skill set

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+// for hamburger navbar
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".navmenu");
+
+hamburger.addEventListener("click" , () =>{
+    hamburger.classList.toggle("active");
+    navmenu.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n. 
+    addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navmenu.classList.remove("active");
+}))
